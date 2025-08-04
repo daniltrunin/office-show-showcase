1c28ccfb944c2f6f1d20b0e7070fd9c572113b82
daniltrunin <ruslantooyoung@gmail.com>
2025-08-04 13:00:01 +0500

    Header component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import './app/styles/index.css'
-import App from '@app/App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './app/styles/index.css'; // глобальные стили
+import './app/styles/variables.css'; // переменные для стилей
+import App from '@app/App';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
-)
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,10 +1,14 @@
 import styles from './Header.module.css';
 
 export default function Header() {
-
-    return (
-        <header className={styles.container}>
-            Header
-        </header>
-    );
+  return (
+    <header className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.details}>
+          <h1 className={styles.title}>Office Show Showcase</h1>
+          <p className={styles.subtitle}>Everything you need to know about The Office</p>
+        </div>
+      </div>
+    </header>
+  );
 }
